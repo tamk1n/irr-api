fcb95509a61b7a5d39e9a3fcc8652c83ac629ec2
Tamkin Tamrazli <109694582+tamk1n@users.noreply.github.com>
2024-05-20 20:47:27 +0400

    Revert "Feat/logging"

--- user_position/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('', UserPositionAPIView.as_view(), name='UserPositionAPIView'),
-    path('<int:id>/', UserPositionDetailView.as_view(), name='UserPositionDetailView'),
-]
